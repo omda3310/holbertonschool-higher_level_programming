b26b3e42a1e47cb66712fb474f7a505a6866ca66
omda3310 <6197@holbertonstudents.com>
2023-06-01 01:12:28 +0200

    print string

--- python-hello_world/5-print_string.py
+++ python-hello_world/5-print_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 str = "Holberton School"
-print(f"{str[0:9]} {str} {str} {str[9:]}")
-print(f"{str[0:9]}")
+print(f"{str[:9]} {str[:9]}{str[9:]} {str[:9]}{str[9:]} {str[9:]}")
+print(f"{str[:9]}")
