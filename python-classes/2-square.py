c457817c538d6bc3223a674c229a06c5a5cb6d76
omda3310 <6197@holbertonstudents.com>
2023-06-15 12:33:02 +0200

    Size validation

---
+++ python-classes/2-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Class in python"""
+
+
+class Square:
+    """Represents an empty square."""
+
+    def __init__(self, size=0):
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
