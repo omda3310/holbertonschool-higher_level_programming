675b926b1e4ff3f2d2b3c14b4b1e8b6e27a56049
omda3310 <6197@holbertonstudents.com>
2023-06-15 14:47:14 +0200

    Integers division with debug

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -3,9 +3,7 @@ def safe_print_division(a, b):
     try:
         div = a / b
     except ZeroDivisionError:
-        print("connot divide by zero")
-        return None
-    else:
-        return div
+        div = None
     finally:
         print("Inside result:{}".format(div))
+    return div
