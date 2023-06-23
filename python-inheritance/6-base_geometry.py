eca3225ad553ab4acd4e5a5b30821439cdab977d
omda3310 <6197@holbertonstudents.com>
2023-06-23 12:03:24 +0200

    Improve Geometry

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Geometry module"""
+
+
+class BaseGeometry:
+
+    """Improve Geometry"""
+    def area(self):
+        """Public instance method"""
+        raise Exception("area() is not implemented")
