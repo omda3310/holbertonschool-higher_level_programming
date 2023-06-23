3b2ffc123c62b968f9b7440c6f3a8639397e9d2f
omda3310 <6197@holbertonstudents.com>
2023-06-24 00:22:09 +0200

    update Exception

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -14,6 +14,6 @@ class BaseGeometry:
         """Public instance method validation"""
 
         if type(value) is not int:
-            raise TypeError(f"{name} must be an integer")
+            raise TypeError(f'{name} must be an integer')
         if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
+            raise ValueError(f'{name} must be greater than 0')
