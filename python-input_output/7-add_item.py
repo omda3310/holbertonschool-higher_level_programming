7a7b820e7782a78f0282cd95da3f965cd5b1891e
omda3310 <6197@holbertonstudents.com>
2023-07-26 19:39:57 +0200

    Load, add, save

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -4,17 +4,16 @@ import sys
 from os.path import exists
 from json import load, dump
 
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+if __name__ == "__main__":
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = __import__
+    ('6-load_from_json_file').load_from_json_file
+    fl = "add_item.json"
+    arg = sys.argv[1:]
 
-fl = "add_item.json"
-arg = sys.argv[1:]
-
-
-if __name__ == '__main__':
-    if exists(fl):
+    try:
         mlist = load_from_json_file(fl)
-    else:
+    except FileNotFoundError:
         mlist = []
 
     mlist.extend(arg)
