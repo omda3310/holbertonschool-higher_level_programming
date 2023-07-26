1e7a743c157ff11acc3a3547e4534725ff512bff
omda3310 <6197@holbertonstudents.com>
2023-07-26 17:40:56 +0200

    Write to a file

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -8,7 +8,7 @@ def append_write(filename="", text=""):
     """This function has two parameters: filename is the name of file and
     text is the string to be writing in the text"""
     
-    with open(filename, mode='w', encoding='utf-8') as filename:
+    with open(filename, 'w', encoding='utf-8') as filename:
         
         """This function uses the with open method to open the file in writing mode(mode='w')
         and utf-8 encoding(mode='utf-8')"""
