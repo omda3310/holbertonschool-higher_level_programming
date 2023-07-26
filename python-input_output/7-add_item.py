e79872d3a7feb378ae84724f64ef37493ca41cf4
omda3310 <6197@holbertonstudents.com>
2023-07-26 19:04:40 +0200

    Load, add, save

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-"""a script that adds all arguments to a Python list,
-and then save them to a file"""
-
-
+"""add all arguments and save them to a file"""
 import sys
 from os.path import exists
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
