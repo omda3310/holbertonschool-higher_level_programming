6addf2519737c97b8f8f49011b68cc2dc247cfc6
omda3310 <6197@holbertonstudents.com>
2023-07-26 21:11:51 +0200

    update

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -12,9 +12,9 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 my_list = []
 fl = "add_item.json"
 
-if os.path.exists("add_item.json"):
-    my_list = load_from_json_file("add_item.json")
+if os.path.exists(fl):
+    my_list = load_from_json_file(fl)
 for i in range(1, len(sys.argv)):
     my_list.append(sys.argv[i])
 """Save the list"""
-save_to_json_file(my_list, "add_item.json")
+save_to_json_file(my_list, fl)
