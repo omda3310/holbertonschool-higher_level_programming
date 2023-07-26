945fb2c2d8f0b931a24d756918de7a8a89e8abf7
omda3310 <6197@holbertonstudents.com>
2023-07-26 17:02:42 +0200

    Read file

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Read file method"""
+
+def read_file(filename=""):
+    """A function that reads a text file (UTF8) and prints it to stdout"""
+    with open(filename, encoding='utf-8') as fl:
+        for l in fl:
+            print(l, end='')
