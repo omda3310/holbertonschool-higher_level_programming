9114cace3f3694c283110bfd5008027e7afe6bb1
omda3310 <6197@holbertonstudents.com>
2023-07-26 19:25:07 +0200

    Load, add, save

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -2,19 +2,18 @@
 """add all arguments and save them to a file"""
 import sys
 from os.path import exists
-from json import dump, load
+from json import load, dump
+
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 fl = "add_item.json"
 arg = sys.argv[1:]
 
-if __name__ == "__main__":
-
-    if exists(fl):
+if exists(fl):
         mlist = load_from_json_file(fl)
-    else:
-        mlist = []
+else:
+    mlist = []
 
-    mlist.extend(arg)
-    save_to_json_file(mlist, fl)
+mlist.extend(arg)
+save_to_json_file(mlist, fl)
