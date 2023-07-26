48fddb58e54f016b28f9c245a70abfe330cfafda
omda3310 <6197@holbertonstudents.com>
2023-07-26 22:38:25 +0200

    update all code

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -2,21 +2,20 @@
 """add_item method"""
 
 import sys
-import json
 from os.path import exists
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-fil = "add_item.json"
+filename = "add_item.json"
 
 
 if __name__ == "__main__":
     """test"""
-    if exists(fil):
-        my_list = load_from_json_file(fil)
+    if exists(filename):
+        my_list = load_from_json_file(filename)
     else:
         my_list = []
     """load file"""
     my_list.extend(sys.argv[1:])
     """Save the list"""
-    save_to_json_file(my_list, fil)
+    save_to_json_file(my_list, filename)
