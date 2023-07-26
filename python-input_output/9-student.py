b19b882bc57434d45a52595a3814b7e802358a05
omda3310 <6197@holbertonstudents.com>
2023-07-26 21:55:07 +0200

    Student to JSON

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -5,11 +5,11 @@
 class Student:
     """Initialisation of class Student"""
     def __init__(self, first_name, last_name, age):
-        
+
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
-    
+
     def to_json(self):
         """Return dictionary representation of Student"""
         return self.__dict__
