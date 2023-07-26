c4794a826d48aa4c997cf9f4ecb99b52246e15e1
omda3310 <6197@holbertonstudents.com>
2023-07-26 18:54:30 +0200

    Load, add, save

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""a script that adds all arguments to a Python list,
+and then save them to a file"""
+
+
+import sys
+from os.path import exists
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+
+if __name__ == "__main__":
+    if exists("add_items.json"):
+        mlist = load_from_json_file("add_item.json")
+    else:
+        mlist = []
+
+    mlist.extend(sys.argv[1:])
+    save_to_json_file(mlist, "add_item.json")
