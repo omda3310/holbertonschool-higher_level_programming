3b721cc8e7dc5645c6deb3e3d0b102c2732631cc
omda3310 <6197@holbertonstudents.com>
2023-07-26 17:38:29 +0200

    Write to a file

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""a function that appends a string at the end of a text file (UTF8) and returns the number of characters added"""
+"""a function that appends a string at the end of a text file (UTF8)
+and returns the number of characters added"""
 
 
 def append_write(filename="", text=""):
@@ -7,9 +8,9 @@ def append_write(filename="", text=""):
     """This function has two parameters: filename is the name of file and
     text is the string to be writing in the text"""
     
-    with open(filename, mode='w', encoding='utf-8') as fl:
+    with open(filename, mode='w', encoding='utf-8') as filename:
         
         """This function uses the with open method to open the file in writing mode(mode='w')
         and utf-8 encoding(mode='utf-8')"""
         
-        return fl.write(text)
+        return filename.write(text)
