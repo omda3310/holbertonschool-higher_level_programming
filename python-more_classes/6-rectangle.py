fe2af16c9604aadb2f6206dbbebf764252da7673
omda3310 <6197@holbertonstudents.com>
2023-08-06 15:34:28 +0200

    How many instances

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -6,6 +6,7 @@ class Rectangle:
     """class Rectangle"""
 
     def __init__(self, width=0, height=0):
+        Rectangle.number_of_instances += 1
         self.width = width
         self.height = height
     """private instance width"""
