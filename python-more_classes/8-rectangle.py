744599dfdf99a3aad5a498e74f722abdcf8c5105
omda3310 <6197@holbertonstudents.com>
2023-08-06 16:11:27 +0200

    Compare rectangles

--- python-more_classes/8-rectangle.py
+++ python-more_classes/8-rectangle.py
@@ -68,16 +68,14 @@ class Rectangle:
     def __del__(self):
         Rectangle.number_of_instances -= 1
         print("Bye rectangle...")
-
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
-        """ Compare two rectangles"""
-
+        """Return the bigger rectangle based on the area"""
         if not isinstance(rect_1, Rectangle):
             raise TypeError("rect_1 must be an instance of Rectangle")
         if not isinstance(rect_2, Rectangle):
-            raise TabError("rect_2 must be an instance of Rectangle")
-        if (rect_1.area() >= rect_2.area()):
+            raise TypeError("rect_2 must be an instance of Rectangle")
+        if rect_1.area() >= rect_2.area():
             return rect_1
         else:
             return rect_2
