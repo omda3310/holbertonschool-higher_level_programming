7c4a24dccd96e735d504b45225671fb6b1bad75c
omda3310 <6197@holbertonstudents.com>
2023-08-06 14:54:49 +0200

    Roman to Integer

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if not roman_string or not isinstance(roman_string, str):
+    if not roman_string is None or not isinstance(roman_string, str):
         return 0
     roman_numbers = {
         "I": 1,
@@ -13,7 +13,7 @@ def roman_to_int(roman_string):
     }
     sum = 0
     for ch in range(len(roman_string)):
-      if ch >0 and roman_numbers[roman_string[ch]] > roman_numbers[roman_string[ch -1]]:
+      if ch > 0 and roman_numbers[roman_string[ch]] > roman_numbers[roman_string[ch -1]]:
           sum += roman_numbers[roman_string[ch]] - 2 * roman_numbers[roman_string[ch -1]]
     else:
         sum += roman_numbers[roman_string[ch]]
