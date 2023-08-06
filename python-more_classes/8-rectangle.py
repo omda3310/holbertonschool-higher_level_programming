1bf4429d2959dfcb5494b4547bde6030b1270c43
omda3310 <6197@holbertonstudents.com>
2023-08-06 16:15:36 +0200

    Compare rectangles

--- python-more_classes/8-rectangle.py
+++ python-more_classes/8-rectangle.py
@@ -68,6 +68,7 @@ class Rectangle:
     def __del__(self):
         Rectangle.number_of_instances -= 1
         print("Bye rectangle...")
+
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
         """Return the bigger rectangle based on the area"""
