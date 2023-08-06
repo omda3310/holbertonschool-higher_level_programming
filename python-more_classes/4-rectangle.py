2ecc9b21cdc6f267a0452c307a8166cc6ff600e3
omda3310 <6197@holbertonstudents.com>
2023-08-06 15:20:30 +0200

    Eval is magic

---
+++ python-more_classes/4-rectangle.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+"""class Rectangle"""
+
+
+class Rectangle:
+    """class Rectangle"""
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+    """private instance width"""
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+    """private instance height"""
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """Area rectangle"""
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """Perimeter rectangle"""
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return (self.__width + self.__height) * 2
+
+    """print rectangle"""
+
+    def __str__(self):
+        if self.__width == 0 or self.__height == 0:
+            return ""
+
+        rectangle = ""
+        for i in range(self.__height):
+            rectangle += "#" * self.__width + "\n"
+        return (rectangle[:-1])
+    
+    def __repr__(self):
+        return ("Rectangle({}, {})".format(self.__width, self.__height))
+    
