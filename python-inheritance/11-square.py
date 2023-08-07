c1b94a75db92866dc1c68d38dd6dba6780b2f899
omda3310 <6197@holbertonstudents.com>
2023-08-07 12:09:25 +0200

    the square description

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""a class Square that inherits from Rectangle"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Initialisation"""
+
+    def __init__(self, size):
+        """Square constructor"""
+        self.integer_validator('size', size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        """Area of Square"""
+        return self.__size * self.__size
+
+    def __str__(self):
+        """the square description"""
+        return "[Square] {}/{}".format(self.__size, self.__size)
