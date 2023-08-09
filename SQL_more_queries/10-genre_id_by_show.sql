7094f18ae2193a5fa4500c09eb7521094db51f4e
omda3310 <6197@holbertonstudents.com>
2023-08-09 11:52:32 +0200

    Genre ID by show

---
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,2 @@
+--  lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
+SELECT s.title, g.genre_id FROM tv_show AS s INNER JOIN tv_show_genres AS g ON s.id = g.genre_id ORDER BY s.title, g.show_id ASC;
