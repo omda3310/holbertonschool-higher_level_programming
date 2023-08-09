ae03c1384c328d2a99d97f858104f364f921fe4f
omda3310 <6197@holbertonstudents.com>
2023-08-09 10:07:03 +0200

    creates the MySQL server user user_0d_1

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
 -- creates the MySQL server user user_0d_1
 create user if not exists 'user_0d_1'@'localhost'
 IDENTIFIED BY 'user_0d_1_pwd';
-GRANT all provileges on *.* to 'user_0d_1'@localhost';
+GRANT all provileges on *.* to 'user_0d_1'@'localhost';
